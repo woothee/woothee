02cf1a00f7b44540d94420b4013891595bd5861e
TAGOMORI Satoshi <tagomoris@gmail.com>
2012-01-16 19:44:24 +0900

    write Crawler/Google

---
+++ java/src/java/is/tagomor/woothee/Classifier.java
@@ -0,0 +1,51 @@
+package is.tagomor.woothee;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+
+import is.tagomor.woothee.DataSet;
+
+public final class Classifier {
+  public static final String ATTRIBUTE_KEY_NAME = "name";
+  public static final String ATTRIBUTE_KEY_CATEGORY = "category";
+  public static final String ATTRIBUTE_KEY_OS = "os";
+  public static final String ATTRIBUTE_KEY_VERSION = "version";
+  public static final String ATTRIBUTE_KEY_VENDOR = "vendor";
+
+  public static final String VALUE_UNKNOWN = "UNKNOWN";
+
+
+  public static Map<String,String> parse(final String useragent) {
+    HashMap<String,String> result = new HashMap<String,String>(5, (float)1.0); // initial capacity, load factor
+
+    if (tryCrawler(useragent, result)) {
+      System.out.println(result.toString());
+      return fillResult(result);
+    }
+
+    return fillResult(result);
+  }
+
+  public static Map<String,String> fillResult(final Map<String,String> result) {
+    if (result.get(ATTRIBUTE_KEY_NAME) == null)
+      result.put(ATTRIBUTE_KEY_NAME, VALUE_UNKNOWN);
+    if (result.get(ATTRIBUTE_KEY_CATEGORY) == null)
+      result.put(ATTRIBUTE_KEY_CATEGORY, VALUE_UNKNOWN);
+    if (result.get(ATTRIBUTE_KEY_OS) == null)
+      result.put(ATTRIBUTE_KEY_OS, VALUE_UNKNOWN);
+    if (result.get(ATTRIBUTE_KEY_VERSION) == null)
+      result.put(ATTRIBUTE_KEY_VERSION, VALUE_UNKNOWN);
+    if (result.get(ATTRIBUTE_KEY_VENDOR) == null)
+      result.put(ATTRIBUTE_KEY_VENDOR, VALUE_UNKNOWN);
+    return result;
+  }
+
+  public static boolean tryCrawler(final String useragent, final Map<String,String> result) {
+    if (is.tagomor.woothee.crawler.Google.challenge(useragent, result)) {
+      return true;
+    }
+    return false;
+  }
+
+}
---
+++ java/src/java/is/tagomor/woothee/crawler/Google.java
@@ -0,0 +1,44 @@
+package is.tagomor.woothee.crawler;
+
+import java.util.Map;
+
+import is.tagomor.woothee.AgentCategory;
+import is.tagomor.woothee.DataSet;
+
+public class Google extends AgentCategory {
+  public static boolean challenge(final String useragent, final Map<String,String> result) {
+    if (useragent.indexOf("Google") < 0)
+      return false;
+    
+    if (useragent.indexOf("compatible; Googlebot") > -1) { // Googlebot or Googlebot-Mobile
+      if (useragent.indexOf("compatible; Googlebot-Mobile") > -1) {
+        updateMap(result, DataSet.get("GoogleBotMobile"));
+        return true;
+      }
+      else {
+        updateMap(result, DataSet.get("GoogleBot"));
+        return true;
+      }
+    }
+    if (useragent.indexOf("Mediapartners-Google") > -1) {
+      if (useragent.indexOf("compatible; Mediapartners-Google") > -1 || useragent.equals("Mediapartners-Google")) {
+        updateMap(result, DataSet.get("GoogleMediaPartners"));
+        return true;
+      }
+    }
+    if (useragent.indexOf("Feedfetcher-Google;") > -1) {
+      updateMap(result, DataSet.get("GoogleFeedFetcher"));
+      return true;
+    }
+    if (useragent.indexOf("AppEngine-Google") > -1) {
+      updateMap(result, DataSet.get("GoogleAppEngine"));
+      return true;
+    }
+    if (useragent.indexOf("Google Web Preview") > -1) {
+      updateMap(result, DataSet.get("GoogleWebPreview"));
+      return true;
+    }
+
+    return false;
+  }
+}
