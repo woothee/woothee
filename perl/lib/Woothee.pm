2b0aed7fc0b1a0adaae1a16ccba0f27d00aded70
TAGOMORI Satoshi <tagomoris@gmail.com>
2012-01-20 13:55:39 +0900

    add perl implementation

---
+++ perl/lib/Woothee.pm
@@ -0,0 +1,21 @@
+package Woothee;
+
+use strict;
+use warnings;
+use Carp;
+
+# use Woothee::DataSet;
+# use Woothee::Classifier;
+# use Woothee::AgentCategory;
+# use Woothee::Browser;
+# use Woothee::OS;
+# use Woothee::MobilePhone;
+# use Woothee::Crawler;
+# use Woothee::Appliance;
+# use Woothee::Misc;
+
+sub parse {
+    return {}
+}
+
+1;
