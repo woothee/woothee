3a0444ff6b2ac348296bc703773772e736553d7c
TAGOMORI Satoshi <tagomoris@gmail.com>
2012-01-21 16:42:56 +0900

    fix bugs of return value

--- java/src/java/is/tagomor/woothee/hive/IsIn.java
+++ java/src/java/is/tagomor/woothee/hive/IsIn.java
@@ -13,12 +13,12 @@ import is.tagomor.woothee.DataSet;
              value="_FUNC_ return TRUE when specified map's ['category'] is equals to one of LIST, else NULL",
              extended="count( _FUNC_(parse_agent(user_agent_string), array('pc', 'mobilephone', 'smartphone', 'appliance')) )")
 public final class IsIn extends UDF {
-  public boolean evaluate(final Map<String,String> m, final List<String> categories) {
+  public Boolean evaluate(final Map<String,String> m, final List<String> categories) {
     String targetCategory = m.get(DataSet.ATTRIBUTE_CATEGORY);
     for (String c : categories) {
       if (targetCategory.equals(c))
-        return true;
+        return Boolean.TRUE;
     }
-    return false;
+    return null;
   }
 }
